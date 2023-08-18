1adfe9c59f35b8cc7f37d94217f0485a08309ba8
Charlie <charlie.sanchez31@gmail.com>
2023-08-18 12:30:47 -0700

    Added recomendations as an option. Might get a silly response.

--- responses.py
+++ responses.py
@@ -3,6 +3,11 @@ import random
 
 def get_response(message):
     p_message = message.lower()
+    mobile_games = ["Fate Grand Order", "Candy Crush", "Angry Birds", "Temple Run",
+                    "World of Tanks Blitz", "Please Touch Some Grass"]
+    if p_message == "!recommend":
+        recommended_game = random.choice(mobile_games)
+        return recommended_game
 
     if p_message == "hello":
         return "Hello there!"
@@ -11,6 +16,6 @@ def get_response(message):
         return str(random.randint(1, 6))
 
     if p_message == "!help":
-        return "`This is a help message that you can modify.`"
+        return "`Type !recommend to receive a suggestion from our lovely bot. Results may vary.`"
 
     return "I don't understand what you wrote."
